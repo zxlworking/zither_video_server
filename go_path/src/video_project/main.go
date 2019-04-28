3bebdca233b8f8c90807244bbf54d5cc378fa4a6
zxlworking <szzhangxiaolong@163.com>
2019-04-29 01:33:43 +0800

    Add receive files
    Signed-off-by: zxlworking <szzhangxiaolong@163.com>

--- go_path/src/video_project/main.go
+++ go_path/src/video_project/main.go
@@ -64,11 +64,11 @@ func main() {
 import (
 	"encoding/json"
 	"fmt"
-	"io"
+	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"os/exec"
-	"strconv"
 	"time"
 	data "video_project/data"
 	"video_project/file"
@@ -76,6 +76,7 @@ import (
 )
 
 var baseProjectDir = "C:\\zxl\\zither_video_server\\go_path\\src\\video_project\\"
+
 var ffmpegBinDir = "C:\\Users\\zxl\\Downloads\\ffmpeg-20190426-4b7166c-win64-static\\bin\\ffmpeg.exe"
 
 //返回一个Router实例
@@ -163,64 +164,126 @@ func getVideoFileList(w http.ResponseWriter, r *http.Request) {
 
 	videoFileInfoResponseBeanResult, _ := json.Marshal(videoFileInfoResponseBean)
 	fmt.Fprint(w, string(videoFileInfoResponseBeanResult))
-	fmt.Printf("response", string(videoFileInfoResponseBeanResult))
+	fmt.Println("response", string(videoFileInfoResponseBeanResult))
 }
 
 func uploadVideoFile(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	fmt.Println("uploadVideoFile start")
-	//把上传的文件存储在内存和临时文件中
-	r.ParseMultipartForm(32 << 20)
-	fmt.Println(r.Form)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("Scheme", r.URL.Scheme)
+	/*
+		r.ParseForm()
+		fmt.Println("uploadVideoFile start")
+		//把上传的文件存储在内存和临时文件中
+		r.ParseMultipartForm(32 << 20)
+		//fmt.Println(r.Form)
+		fmt.Println("path", r.URL.Path)
+		fmt.Println("Scheme", r.URL.Scheme)
 
+		videoName := r.URL.Query()["video_name"][0]
+		videoDesc := r.URL.Query()["video_desc"][0]
+		userId := r.URL.Query()["user_id"][0]
+
+		fmt.Println(time.Now().UnixNano())
+		videoRealName := strconv.FormatInt(time.Now().UnixNano(), 10)
+		fmt.Println(videoRealName)
+
+		//获取文件句柄，然后对文件进行存储等处理
+		file, fileHandler, err := r.FormFile("file")
+		if err != nil {
+			fmt.Println("form file err: ", err)
+			return
+		}
+		defer file.Close()
+		//fmt.Fprintf(w, "%v", handler.Header)
+		//创建上传的目的文件
+		f, err := os.OpenFile("./video_file/"+videoRealName, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println("open file err: ", err)
+			return
+		}
+		defer f.Close()
+		//拷贝文件
+		io.Copy(f, file)
+
+		videoFileInfo := data.VideoFileInfo{videoRealName, videoName, videoDesc, userId, "0"}
+		result := video_file.AddVideoFile(videoFileInfo)
+
+		fmt.Println("main::add_video_file::result = ", result)
+
+		var response data.ResponseBaseBean
+		if result == 0 {
+			response = data.ResponseBaseBean{result, "success"}
+		} else {
+			response = data.ResponseBaseBean{result, "fail"}
+		}
+
+		responseResult, error := json.Marshal(response)
+		if error != nil {
+
+		}
+		fmt.Println("main::add_video_file::success = ", string(responseResult))
+		fmt.Fprint(w, string(responseResult))
+
+		go mp4toflv(videoRealName)
+	*/
+
+	r.ParseForm()
 	videoName := r.URL.Query()["video_name"][0]
 	videoDesc := r.URL.Query()["video_desc"][0]
 	userId := r.URL.Query()["user_id"][0]
+	fmt.Println("videoName", videoName)
+	fmt.Println("videoDesc", videoDesc)
+	fmt.Println("userId", userId)
 
-	fmt.Println(time.Now().UnixNano())
-	videoRealName := strconv.FormatInt(time.Now().UnixNano(), 10)
-	fmt.Println(videoRealName)
+	/**
+	底层通过调用multipartReader.ReadForm来解析
+	如果文件大小超过maxMemory,则使用临时文件来存储multipart/form中文件数据
+	*/
+	r.ParseMultipartForm(32 << 20)
+	//fmt.Println("r.Form:         ", r.Form)
+	//fmt.Println("r.PostForm:     ", r.PostForm)
+	//fmt.Println("r.MultiPartForm:", r.MultipartForm)
+	getFormData(r.MultipartForm)
+}
 
-	//获取文件句柄，然后对文件进行存储等处理
-	file, _, err := r.FormFile("file")
-	if err != nil {
-		fmt.Println("form file err: ", err)
-		return
+func getFormData(form *multipart.Form) {
+	/*
+		//获取 multi-part/form body中的form value
+		for k, v := range form.Value {
+			//fmt.Println("value,k,v = ", k, ",", v)
+			fmt.Println("getFormData::form.Value::k = ", k, ",", len(v))
+
+			if len(v) > 0 {
+				dstFile, err := os.Create("./video_file/" + k)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+				defer dstFile.Close()
+				dstFile.WriteString(v[0])
+			}
+		}
+		fmt.Println()
+	*/
+
+	for k, v := range form.File {
+		//fmt.Println("value,k,v = ", k, ",", v)
+		fmt.Println("getFormData::form.File::k = ", k, ",", len(v))
+
+		for _, value := range v {
+			fmt.Println("getFormData::form.File::FileName = ", value.Filename)
+			f, _ := value.Open()
+			buf, _ := ioutil.ReadAll(f)
+
+			dstFile, err := os.Create("./video_file/" + value.Filename)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			defer dstFile.Close()
+			dstFile.WriteString(string(buf))
+		}
 	}
-	defer file.Close()
-	//fmt.Fprintf(w, "%v", handler.Header)
-	//创建上传的目的文件
-	f, err := os.OpenFile("./video_file/"+videoRealName, os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil {
-		fmt.Println("open file err: ", err)
-		return
-	}
-	defer f.Close()
-	//拷贝文件
-	io.Copy(f, file)
+	fmt.Println()
 
-	videoFileInfo := data.VideoFileInfo{videoRealName, videoName, videoDesc, userId, "0"}
-	result := video_file.AddVideoFile(videoFileInfo)
-
-	fmt.Println("main::add_video_file::result = ", result)
-
-	var response data.ResponseBaseBean
-	if result == 0 {
-		response = data.ResponseBaseBean{result, "success"}
-	} else {
-		response = data.ResponseBaseBean{result, "fail"}
-	}
-
-	responseResult, error := json.Marshal(response)
-	if error != nil {
-
-	}
-	fmt.Println("main::add_video_file::success = ", string(responseResult))
-	fmt.Fprint(w, string(responseResult))
-
-	go mp4toflv(videoRealName)
 }
 
 func playVideoFile(w http.ResponseWriter, r *http.Request) {
